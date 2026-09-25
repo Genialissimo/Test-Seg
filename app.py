260ab83ad3a1025c0a08e5a0afe3c163bfa68a6c
Genialissimo <129745843+Genialissimo@users.noreply.github.com>
2026-09-25 09:32:02 +0200

    Update program link for Gestione Registrazioni

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ with st.sidebar:
 
     # Inserisci qui i link ufficiali dei tuoi programmi
     programmi = {
-        "Gestione Registrazioni": "https://gestioneseg.streamlit.app/",
+        "Gestione Registrazioni Segretario": "https://gestioneseg.streamlit.app/",
         "Gestione Programmi": (
             "https://gestione-programmi-7kb2cuwy6ntgwe7kufezrg.streamlit.app/"
         ),
